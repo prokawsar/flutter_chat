41ed78cb4d4e8e03d7e48807f6d9744b8e44e3dc
Kawsar Ahmed <prokawsar@gmail.com>
2022-03-17 07:19:16 -0400

    first

---
+++ android/app/src/main/kotlin/prokawsar/com/flutter_chat/MainActivity.kt
@@ -0,0 +1,6 @@
+package prokawsar.com.flutter_chat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chat/screens/chat_screen.dart';
+import 'package:flutter_chat/screens/login_screen.dart';
+import 'package:flutter_chat/screens/registration_screen.dart';
+import 'package:flutter_chat/screens/welcome_screen.dart';
+
+void main() => runApp(const FlashChat());
+
+class FlashChat extends StatelessWidget {
+  const FlashChat({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: const TextTheme(
+          bodyText1: TextStyle(color: Colors.black54),
+        ),
+      ),
+      // home: const WelcomeScreen(),
+      initialRoute: '/',
+      routes: {
+        // When navigating to the "/" route, build the FirstScreen widget.
+        WelcomeScreen.id: (context) => const WelcomeScreen(),
+        // When navigating to the "/second" route, build the SecondScreen widget.
+        LoginScreen.id: (context) => const LoginScreen(),
+        RegistrationScreen.id: (context) => const RegistrationScreen(),
+        ChatScreen.id: (context) => const ChatScreen(),
+      },
+    );
+  }
+}
